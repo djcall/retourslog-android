37898420827d7c6a5357d9e39076d0934a8778d8
djcall <cedricgazda@gmail.com>
2025-08-24 09:11:37 +0200

    Delete docker_build_apk.bat

--- docker_build_apk.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set IMAGE=kivy/buildozer
-echo [*] Pull image %IMAGE% …
-docker pull %IMAGE%
-echo [*] Building APK (debug)…
-docker run --rm -v "%cd%":/home/user/app -w /home/user/app %IMAGE% buildozer android debug
-echo.
-echo [*] Done. APK should be in .\bin\
-pause
